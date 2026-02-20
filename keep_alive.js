6ef436e0da1827e2e50e4a717632504be41f713a
rimi28-cpu <rimiikarashi@gmail.com>
2026-02-20 19:04:50 +0530

    Create keep_alive.js

---
+++ keep_alive.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('Bot is alive!'));
+app.listen(port, () => console.log(`Keep-alive server running on port ${port}`));
